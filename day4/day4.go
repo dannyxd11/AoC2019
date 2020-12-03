6b1195e60f08e465323edf00095a3a540a88c0a0
Dan <dannyxd@me.com>
2020-12-03 16:12:29 +0000

    Removing some logging / cleanup

--- day4/day4.go
+++ day4/day4.go
@@ -13,11 +13,11 @@ func check(e error) {
 	}
 }
 
-func convertFloatToIntArray(current float64) []int{
+func convertFloatToIntArray(current float64) []int {
 	scurrent := strconv.FormatFloat(current, 'f', -1, 64)
 	sacurrent := strings.Split(scurrent, "")
 
-	var acurrent []int;
+	var acurrent []int
 	for _, v := range sacurrent {
 		i, err := strconv.Atoi(v)
 		check(err)
@@ -27,10 +27,10 @@ func convertFloatToIntArray(current float64) []int{
 	return acurrent
 }
 
-func convertIntArrayToFloat(current []int) float64{
-	val, err := strconv.ParseFloat(strings.Trim(strings.Join(strings.Fields(fmt.Sprint(current)), ""),"[]"), 8);
+func convertIntArrayToFloat(current []int) float64 {
+	val, err := strconv.ParseFloat(strings.Trim(strings.Join(strings.Fields(fmt.Sprint(current)), ""), "[]"), 8)
 	check(err)
-	return val;
+	return val
 }
 
 func lastMax(array []int) (max int, maxi int) {
@@ -58,24 +58,25 @@ func firstMin(array []int) (min int, mini int) {
 	return
 }
 
-func isValidPwd(current float64, mode int) (isValid bool){
-	if mode == 1{
+func isValidPwd(current float64, mode int) (isValid bool) {
+	switch {
+	case mode == 1:
 		return isValidPwdP1(current)
-	}else if mode == 2{
+	case mode == 2:
 		return isValidPwdP2(current)
-	}else{
+	default:
 		panic("Uh, what?")
 	}
 }
 
-func isValidPwdP1(current float64)(isValid bool){
+func isValidPwdP1(current float64) (isValid bool) {
 	pwd := convertFloatToIntArray(current)
-	_,mini := firstMin(pwd)
+	_, mini := firstMin(pwd)
 	if mini > 0 {
 		return false
 	}
-	_,maxi := lastMax(pwd)
-	if maxi < len(pwd) - 1  {
+	_, maxi := lastMax(pwd)
+	if maxi < len(pwd)-1 {
 		return false
 	}
 
@@ -87,35 +88,33 @@ func isValidPwdP1(current float64)(isValid bool){
 	return false
 }
 
-func isValidPwdP2(current float64)(isValid bool){
+func isValidPwdP2(current float64) (isValid bool) {
 	pwd := convertFloatToIntArray(current)
-	_,mini := firstMin(pwd)
+	_, mini := firstMin(pwd)
 	if mini > 0 {
 		return false
 	}
-	_,maxi := lastMax(pwd)
-	if maxi < len(pwd) - 1  {
+	_, maxi := lastMax(pwd)
+	if maxi < len(pwd)-1 {
 		return false
 	}
 
 	for i := 0; i < len(pwd); i++ {
-		consec_counter := 1
-		//fmt.Println(consec_counter, pwd, i)
-		for j := 1; j + i < len(pwd); j++ {
-			//fmt.Println(consec_counter, pwd, i,j )
-			if pwd[i] == pwd[i + j] {
-				consec_counter += 1
-				if i + j == len(pwd) - 1 && consec_counter % 2 != 0 {
-					return false;
+		consecCounter := 1
+		for j := 1; j+i < len(pwd); j++ {
+			if pwd[i] == pwd[i+j] {
+				consecCounter += 1
+				if i+j == len(pwd)-1 && consecCounter%2 != 0 {
+					return false
 				}
 			} else {
 				break
 			}
 		}
-		if consec_counter > 1 && consec_counter == 2 {
+		if consecCounter > 1 && consecCounter == 2 {
 			return true
 		} else {
-			i += consec_counter - 1
+			i += consecCounter - 1
 		}
 
 	}
@@ -123,21 +122,15 @@ func isValidPwdP2(current float64)(isValid bool){
 	return false
 }
 
-func generateNext(fcurrent float64)(fnext float64, changed bool){
+func generateNext(fcurrent float64) (fnext float64, changed bool) {
 
 	current := convertFloatToIntArray(fcurrent)
-	changed = false;
+	changed = false
 
 	next := make([]int, len(current))
 	copy(next, current)
 
 	min, mini := firstMin(current)
-	log.WithFields(log.Fields{
-		"min":        min,
-		"mini":       mini,
-		"current":    current,
-		"next": next,
-	}).Trace("After min")
 
 	if mini != 0 {
 		changed = true
@@ -146,20 +139,7 @@ func generateNext(fcurrent float64)(fnext float64, changed bool){
 		}
 	}
 
-	log.WithFields(log.Fields{
-		"min":        min,
-		"mini":       mini,
-		"current":    current,
-		"next": next,
-	}).Trace("After change min")
-
 	max, maxi := lastMax(next)
-	log.WithFields(log.Fields{
-		"max":        max,
-		"maxi":       maxi,
-		"current":    current,
-		"next": next,
-	}).Trace("After max")
 
 	if maxi != len(next)-1 {
 		changed = true
@@ -167,45 +147,33 @@ func generateNext(fcurrent float64)(fnext float64, changed bool){
 			next[i] = max
 		}
 	}
-	log.WithFields(log.Fields{
-		"max": max,
-		"maxi": maxi,
-		"current": current,
-		"next": next,
-	}).Trace("After change max")
 
 	fnext = convertIntArrayToFloat(next)
 	return
 }
 
-//271973
 func nextValidPwd(current float64, mode int) (next float64) {
 
-	var isValid bool = false
-	var changed bool = false
+	var isValid = false
+	var changed = false
 	next = current
-	for true{
+	for true {
 		next, changed = generateNext(next)
-		isValid = isValidPwd(next,mode)
-		log.WithFields(log.Fields{
-			"current": current,
-			"next": next,
-			"isValid": isValid,
-			"changed": changed,
-		}).Debug("IsValid/Changed")
+		isValid = isValidPwd(next, mode)
+
 		if isValid && changed {
 			return next
-		} else if isValidPwd(next+1,mode){
+		} else if isValidPwd(next+1, mode) {
 			return next + 1
 		} else {
-			next += 1
+			next++
 		}
 	}
 
 	return next
 }
 
-func part1(lower float64, upper float64) int{
+func part1(lower float64, upper float64) int {
 	current := lower
 	counter := 0
 	for current <= upper {
@@ -213,23 +181,23 @@ func part1(lower float64, upper float64) int{
 			"current": current,
 			"counter": counter,
 		}).Debug("current")
-		current = nextValidPwd(current,1)
+		current = nextValidPwd(current, 1)
 		counter += 1
 	}
 	return counter - 1
 }
 
-func part2(lower float64, upper float64) int{
+func part2(lower float64, upper float64) int {
 	current := lower
 	counter := 0
 	for current <= upper {
-	//for i :=0; i<100;i++ {
+		//for i :=0; i<100;i++ {
 		log.WithFields(log.Fields{
 			"current": current,
 			"counter": counter,
 		}).Debug("current")
-		current = nextValidPwd(current,2)
-		counter += 1
+		current = nextValidPwd(current, 2)
+		counter++
 	}
 	return counter - 1
 }
@@ -238,11 +206,11 @@ func main() {
 	log.SetLevel(log.InfoLevel)
 
 	log.WithFields(log.Fields{
-		"Valid Count": part1(271973,785961),
+		"Valid Count": part1(271973, 785961),
 	}).Info("Part 1")
 
 	log.WithFields(log.Fields{
-		"Valid Count": part2(271973,785961),
+		"Valid Count": part2(271973, 785961),
 	}).Info("Part 2")
 
 }
